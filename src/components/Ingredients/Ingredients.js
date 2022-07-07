6caf271c3cff303474eccb798890b994d23bbaad
dsampath <dsampath@wiley.com>
2022-07-07 20:47:12 +0530

    Loading indicator introduced while adding and deleting items

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,8 +6,10 @@ import IngredientList from "./IngredientList";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const addIngredientHandler = ingredient => {
+      setIsLoading(true);
       //fetch returns a promise. the function inside the then execute only when fetch function successfully completed
       fetch('https://react-prep-b4fd7-default-rtdb.firebaseio.com/ingredient.json',{
           method: 'POST',
@@ -16,6 +18,7 @@ const Ingredients = () => {
               'Content-Type': 'application/json'
           }
       }).then(response => {
+          setIsLoading(false);
           return response.json();//this method gets the body and converted to normal javascript code/ once return
           // the json it goes to next then block
       }).then(responseData => {
@@ -27,9 +30,11 @@ const Ingredients = () => {
   }
 
   const removeIngredientHandler = ingredientId => {
+      setIsLoading(true);
       fetch(`https://react-prep-b4fd7-default-rtdb.firebaseio.com/ingredient/${ingredientId}.json`,{
           method: 'DELETE'
       }).then(response => {
+          setIsLoading(false);
           setUserIngredients(prevIngredients =>
               prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
           );
@@ -42,7 +47,7 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
