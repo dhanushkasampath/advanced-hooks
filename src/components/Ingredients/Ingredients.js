0ae4675bab490b5f26677170ee27ccc157749a0e
dsampath <dsampath@wiley.com>
2022-07-08 14:18:50 +0530

    added httpReducer to handle isLoading and error state variables at once. alternative to useState()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,15 +1,17 @@
-import React, {useCallback, useReducer, useState} from 'react';
+import React, {useCallback, useReducer} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 
+// using reducers is much cleaner/easy to understand. because all your updating logics are in one place
+
 //this is our reducer function. we have to send this to useReducer()
 //Reducer method by default take 2 arguments.
 // 1. current state 2. action object
 const ingredientReducer = (currentIngredients, action) => {
-    switch(action.type){
+    switch(action.type){//these are the multiple ways of changing the state
         case 'SET':
             return action.ingredients;
         case 'ADD':
@@ -21,16 +23,34 @@ const ingredientReducer = (currentIngredients, action) => {
     }
 }
 
+const httpReducer = (currentHttpState, action) => {
+    switch (action.type){//here we register new states that can exists
+        case 'SEND':
+            return { loading: true, error: null };
+        case 'RESPONSE':
+            return { ...currentHttpState, loading: false};//here override the loading property only
+        case 'ERROR':
+            return { loading: false, error: action.errorMessage}//we can use any thing here instead of errorData.
+        case 'CLEAR':
+            return { ...currentHttpState, error: null};
+        default:
+            throw new Error('Should not get there!')
+    }
+}
+
 const Ingredients = () => {
   //when working with useReducer(), react will re-render the component whenever your reducer returns the new state
   const [userIngredients, myDispatch] = useReducer(ingredientReducer, []);//useReducer takes 2 arguments, 1. reducer
   // function 2. starting state
   // const [userIngredients, setUserIngredients] = useState([]); //we replaced this useState by useReducer
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
+
+  const [httpState, myHttpDispatch] = useReducer(httpReducer, { loading: false, error: null });
 
   const addIngredientHandler = ingredient => {
-      setIsLoading(true);
+      // setIsLoading(true);
+      myHttpDispatch({type: 'SEND'});//here only type is needed. action no need as not used in this switch case
       //fetch returns a promise. the function inside the then execute only when fetch function successfully completed
       fetch('https://react-prep-b4fd7-default-rtdb.firebaseio.com/ingredient.json',{
           method: 'POST',
@@ -39,7 +59,9 @@ const Ingredients = () => {
               'Content-Type': 'application/json'
           }
       }).then(response => {
-          setIsLoading(false);
+          // setIsLoading(false);
+          myHttpDispatch({type: 'RESPONSE'});//this line tell react to how to manage the loading state when response
+          // came
           return response.json();//this method gets the body and converted to normal javascript code/ once return
           // the json it goes to next then block
       }).then(responseData => {
@@ -55,11 +77,13 @@ const Ingredients = () => {
   }
 
   const removeIngredientHandler = ingredientId => {
-      setIsLoading(true);
+      // setIsLoading(true);
+      myHttpDispatch({type: 'SEND'});
       fetch(`https://react-prep-b4fd7-default-rtdb.firebaseio.com/ingredient/${ingredientId}.json`,{
           method: 'DELETE'
       }).then(response => {
-          setIsLoading(false);
+          // setIsLoading(false);
+          myHttpDispatch({type: 'RESPONSE'});
           // setUserIngredients(prevIngredients =>
           //     prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
           // );
@@ -69,8 +93,9 @@ const Ingredients = () => {
           })
       }).catch(error => {
           // setError('Something went wrong!');
-          setError(error.message);
-          setIsLoading(false);
+          // setError(error.message);
+          // setIsLoading(false);
+          myHttpDispatch({type: 'ERROR', errorMessage: 'Something went wrong!'});
       });
   }
 
@@ -81,13 +106,13 @@ const Ingredients = () => {
   }, []);
 
   const clearError = () => {
-      setError(null);
+      myHttpDispatch({type: 'CLEAR'});
   }
 
   return (
     <div className="App">
-        {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
+        {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
